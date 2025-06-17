d92048f1ed2955e0bcf3eccb26c5ca4ed8e15a2d
frannpineroo <frannpinero@hotmail.com>
2025-06-17 18:20:53 -0300

    aniado controllers para conectar el back con el front

---
+++ Proyecto2025.Server/Proyecto2025.Server/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Proyecto2025.BD.Datos;
+using Proyecto2025.BD.Datos.Entity;
+
+namespace Proyecto2025.Server.Controllers
+{
+    [ApiController]
+    [Route("api/Usuario")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        public UsuarioController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("GetAll")] //api/Usuario/GetAll
+        public async Task<ActionResult<List<User>>> GetUser();
+        {
+            
+        }
+    }
+}
