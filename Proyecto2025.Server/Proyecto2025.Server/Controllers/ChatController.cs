206b3ff8598e92c21ffe5f89dacb1f5e119b1aa2
ezequiel7888 <ezequieljavier172@gmail.com>
2025-06-22 20:37:44 -0300

    chat y chat members actualizados

--- Proyecto2025.Server/Proyecto2025.Server/Controllers/ChatController.cs
+++ Proyecto2025.Server/Proyecto2025.Server/Controllers/ChatController.cs
@@ -102,46 +102,7 @@ namespace Proyecto2025.Server.Controllers
             }
             return Ok(chat);
         }
-
-        [HttpPost("{Name}")]
-        public async Task<ActionResult<long>> PostChatByName(string Name, Chat DTO)
-        {
-            try
-            {
-                DTO.Name = Name; // Asignar el nombre al DTO
-                await context.Chats.AddAsync(DTO);
-                await context.SaveChangesAsync();
-                return Ok(DTO.Id);
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Error al crear el chat: {e.Message}");
-            }
-        }
-
-        [HttpPut("por-nombre/{Name}")]
-
-        public async Task<ActionResult> PutChatByName(string Name, Chat DTO)
-        {
-            var chat = await context.Chats.FirstOrDefaultAsync(c => c.Name == Name);
-            if (chat == null)
-            {
-                return NotFound("Chat no encontrado.");
-            }
-            try
-            {
-                chat.Name = DTO.Name; // Actualizar el nombre del chat
-                chat.Messages = DTO.Messages; // Actualizar los mensajes del chat
-                context.Chats.Update(chat);
-                await context.SaveChangesAsync();
-                return Ok("Chat actualizado correctamente.");
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Error al actualizar el chat: {e.Message}");
-            }
-        }
-
+         
         [HttpDelete("por-nombre/{Name}")]
         public async Task<ActionResult> DeleteChatByName(string Name)
         {
@@ -163,7 +124,6 @@ namespace Proyecto2025.Server.Controllers
         }
         #endregion
 
-
         #region
         [HttpGet("por-grupo/{IsGroup}")]
 
@@ -176,66 +136,7 @@ namespace Proyecto2025.Server.Controllers
             }
             return Ok(chats);
         }
-
-
-        [HttpPost("por-grupo/{IsGroup}")]
-        public async Task<ActionResult<long>> PostChatByGroupStatus(bool IsGroup, Chat DTO)
-        {
-            try
-            {
-                DTO.IsGroup = IsGroup; // Asignar el estado de grupo al DTO
-                await context.Chats.AddAsync(DTO);
-                await context.SaveChangesAsync();
-                return Ok(DTO.Id);
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Error al crear el chat: {e.Message}");
-            }
-
-        }
-
-        [HttpPut("por-grupo/{IsGroup}")]
-        public async Task<ActionResult> PutChatByGroupStatus(bool IsGroup, Chat DTO)
-        {
-            var chat = await context.Chats.FirstOrDefaultAsync(c => c.IsGroup == IsGroup);
-            if (chat == null)
-            {
-                return NotFound("Chat no encontrado.");
-            }
-            try
-            {
-                chat.IsGroup = DTO.IsGroup; // Actualizar el estado de grupo del chat
-                context.Chats.Update(chat);
-                await context.SaveChangesAsync();
-                return Ok("Chat actualizado correctamente.");
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Error al actualizar el chat: {e.Message}");
-            }
-        }
-        [HttpDelete("por-grupo/{IsGroup}")]
-        public async Task<ActionResult> DeleteChatByGroupStatus(bool IsGroup)
-        {
-            var chat = await context.Chats.FirstOrDefaultAsync(c => c.IsGroup == IsGroup);
-            if (chat == null)
-            {
-                return NotFound("Chat no encontrado.");
-            }
-            try
-            {
-                context.Chats.Remove(chat);
-                await context.SaveChangesAsync();
-                return Ok("Chat eliminado correctamente.");
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Error al eliminar el chat: {e.Message}");
-            }
-        }
         #endregion
-
 
         #region
         [HttpGet("por-moderador/{IsModerated}")]
@@ -247,67 +148,6 @@ namespace Proyecto2025.Server.Controllers
                 return NotFound("No se encontraron chats con el estado de moderación especificado.");
             }
             return Ok(chats);
-        }
-
-        [HttpPost("por-moderador/{IsModerated}")]
-
-        public async Task<ActionResult<long>> PostChatByModerationStatus(bool IsModerated, Chat DTO)
-        {
-            try
-            {
-                DTO.IsModerated = IsModerated; // Asignar el estado de moderación al DTO
-                await context.Chats.AddAsync(DTO);
-                await context.SaveChangesAsync();
-                return Ok(DTO.Id);
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Error al crear el chat: {e.Message}");
-            }
-        }
-
-        [HttpPut("por-moderador/{IsModerated}")]
-
-        public async Task<ActionResult> PutChatByModerationStatus(bool IsModerated, Chat DTO)
-        {
-            var chat = await context.Chats.FirstOrDefaultAsync(c => c.IsModerated == IsModerated);
-            if (chat == null)
-            {
-                return NotFound("Chat no encontrado.");
-            }
-            try
-            {
-                chat.IsModerated = DTO.IsModerated; // Actualizar el estado de moderación del chat
-                context.Chats.Update(chat);
-                await context.SaveChangesAsync();
-                return Ok("Chat actualizado correctamente.");
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Error al actualizar el chat: {e.Message}");
-            }
-        }
-
-        [HttpDelete("por-moderador/{IsModerated}")]
-
-        public async Task<ActionResult> DeleteChatByModerationStatus(bool IsModerated)
-        {
-            var chat = await context.Chats.FirstOrDefaultAsync(c => c.IsModerated == IsModerated);
-            if (chat == null)
-            {
-                return NotFound("Chat no encontrado.");
-            }
-            try
-            {
-                context.Chats.Remove(chat);
-                await context.SaveChangesAsync();
-                return Ok("Chat eliminado correctamente.");
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Error al eliminar el chat: {e.Message}");
-            }
-
         }
         #endregion
 
@@ -322,63 +162,6 @@ namespace Proyecto2025.Server.Controllers
             }
             return Ok(chats);
         }
-
-        [HttpPost("por-fechadecreacion/{CreatedAt}")]
-        public async Task<ActionResult<long>> PostChatByCreationDate(DateTime CreatedAt, Chat DTO)
-        {
-            try
-            {
-                DTO.CreatedAt = CreatedAt; // Asignar la fecha de creación al DTO
-                await context.Chats.AddAsync(DTO);
-                await context.SaveChangesAsync();
-                return Ok(DTO.Id);
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Error al crear el chat: {e.Message}");
-            }
-        }
-        [HttpPut("por-fechadecreacion/{CreatedAt}")]
-        public async Task<ActionResult> PutChatByCreationDate(DateTime CreatedAt, Chat DTO)
-        {
-            var chat = await context.Chats.FirstOrDefaultAsync(c => c.CreatedAt.Date == CreatedAt.Date);
-            if (chat == null)
-            {
-                return NotFound("Chat no encontrado.");
-            }
-            try
-            {
-                chat.CreatedAt = DTO.CreatedAt; // Actualizar la fecha de creación del chat
-                context.Chats.Update(chat);
-                await context.SaveChangesAsync();
-                return Ok("Chat actualizado correctamente.");
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Error al actualizar el chat: {e.Message}");
-            }
-        }
-
-        [HttpDelete("por-fechadecreacion/{CreatedAt}")]
-        public async Task<ActionResult> DeleteChatByCreationDate(DateTime CreatedAt)
-        {
-            var chat = await context.Chats.FirstOrDefaultAsync(c => c.CreatedAt.Date == CreatedAt.Date);
-            if (chat == null)
-            {
-                return NotFound("Chat no encontrado.");
-            }
-            try
-            {
-                context.Chats.Remove(chat);
-                await context.SaveChangesAsync();
-                return Ok("Chat eliminado correctamente.");
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Error al eliminar el chat: {e.Message}");
-            }
-        }
-
         #endregion
 
         #region
@@ -392,66 +175,8 @@ namespace Proyecto2025.Server.Controllers
             }
             return Ok(chats);
         }
-
-        [HttpPost("por-fechadeactualizacion/{UpdatedAt}")]
-        public async Task<ActionResult<long>> PostChatByUpdateDate(DateTime UpdatedAt, Chat DTO)
-        {
-            try
-            {
-                DTO.UpdatedAt = UpdatedAt; // Asignar la fecha de actualización al DTO
-                await context.Chats.AddAsync(DTO);
-                await context.SaveChangesAsync();
-                return Ok(DTO.Id);
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Error al crear el chat: {e.Message}");
-            }
-        }
-        [HttpPut("por-fechadeactualizacion/{UpdatedAt}")]
-        public async Task<ActionResult> PutChatByUpdateDate(DateTime UpdatedAt, Chat DTO)
-        {
-            var chat = await context.Chats.FirstOrDefaultAsync(c => c.UpdatedAt.Date == UpdatedAt.Date);
-            if (chat == null)
-            {
-                return NotFound("Chat no encontrado.");
-            }
-            try
-            {
-                chat.UpdatedAt = DTO.UpdatedAt; // Actualizar la fecha de actualización del chat
-                context.Chats.Update(chat);
-                await context.SaveChangesAsync();
-                return Ok("Chat actualizado correctamente.");
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Error al actualizar el chat: {e.Message}");
-            }
-        }
-
-        [HttpDelete("por-fechadeactualizacion/{UpdatedAt}")]
-        public async Task<ActionResult> DeleteChatByUpdateDate(DateTime UpdatedAt)
-        {
-            var chat = await context.Chats.FirstOrDefaultAsync(c => c.UpdatedAt.Date == UpdatedAt.Date);
-            if (chat == null)
-            {
-                return NotFound("Chat no encontrado.");
-            }
-            try
-            {
-                context.Chats.Remove(chat);
-                await context.SaveChangesAsync();
-                return Ok("Chat eliminado correctamente.");
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Error al eliminar el chat: {e.Message}");
-            }
-        }
         #endregion
 
-        
     }
 }
-       
  
--- Proyecto2025.Server/Proyecto2025.Server/Controllers/ChatMemberController.cs
+++ Proyecto2025.Server/Proyecto2025.Server/Controllers/ChatMemberController.cs
@@ -33,7 +33,7 @@ namespace Proyecto2025.Server.Controllers
             return Ok(members);
         }
 
-        [HttpPost("por-id/{Id}")]
+        [HttpPost]
         public async Task<ActionResult<ChatMember>> GetChatMemberById(long id)
         {
             var member = await context.ChatMembers
@@ -94,68 +94,6 @@ namespace Proyecto2025.Server.Controllers
         #endregion
 
         #region
-        [HttpGet("por chatid/{ChatId}")]
-        public IActionResult GetChatMember(long id)
-        {
-            var chatMember = context.ChatMembers.FirstOrDefault(cm => cm.Id == id);
-            if (chatMember == null)
-            {
-                return NotFound("Miembro de chat no encontrado.");
-            }
-            return Ok(chatMember);
-        }
-
-        [HttpPost("por-chatid/{ChatId}")]
-        public async Task<ActionResult<ChatMember>> CreateChatMember(ChatMember chatMember)
-        {
-            if (chatMember == null)
-            {
-                return BadRequest("El miembro del chat no puede ser nulo.");
-            }
-            context.ChatMembers.Add(chatMember);
-            await context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetChatMember), new { id = chatMember.Id }, chatMember);
-        }
-        [HttpPut("por-chatid/{ChatId}")]
-        public async Task<IActionResult> UpdateChatMemberChatId(long id, ChatMember chatMember)
-        {
-            if (id != chatMember.Id)
-            {
-                return BadRequest("El ID del miembro del chat no coincide.");
-            }
-            context.Entry(chatMember).State = EntityState.Modified;
-            try
-            {
-                await context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ChatMemberExists(id))
-                {
-                    return NotFound("Miembro de chat no encontrado.");
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return NoContent();
-        }
-        [HttpDelete("por-chatid/{ChatId}")]
-        public async Task<IActionResult> DeleteChatMemberChatId(long id)
-        {
-            var chatMember = await context.ChatMembers.FindAsync(id);
-            if (chatMember == null)
-            {
-                return NotFound("Miembro de chat no encontrado.");
-            }
-            context.ChatMembers.Remove(chatMember);
-            await context.SaveChangesAsync();
-            return NoContent();
-        }
-        #endregion
-
-        #region
 
         [HttpGet("por-chat/{Chat}")]
         public async Task<ActionResult<IEnumerable<ChatMember>>> GetChatMembersByChat(long chatId)
@@ -169,18 +107,6 @@ namespace Proyecto2025.Server.Controllers
                 return NotFound("No members found for this chat.");
             }
             return Ok(members);
-        }
-
-        [HttpPost("por-chat/{Chat}")]
-        public async Task<ActionResult<ChatMember>> CreateChatMemberByChat(ChatMember chatMember)
-        {
-            if (chatMember == null)
-            {
-                return BadRequest("El miembro del chat no puede ser nulo.");
-            }
-            context.ChatMembers.Add(chatMember);
-            await context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetChatMembersByChat), new { chatId = chatMember.ChatId }, chatMember);
         }
 
         [HttpPut("por-chat/{Chat}")]
@@ -208,20 +134,6 @@ namespace Proyecto2025.Server.Controllers
             }
             return NoContent();
         }
-
-        [HttpDelete("por-chat/{Chat}")]
-        public async Task<IActionResult> DeleteChatMemberByChat(long id)
-        {
-            var chatMember = await context.ChatMembers.FindAsync(id);
-            if (chatMember == null)
-            {
-                return NotFound("Miembro de chat no encontrado.");
-            }
-            context.ChatMembers.Remove(chatMember);
-            await context.SaveChangesAsync();
-            return NoContent();
-
-        }
         #endregion
 
         #region
@@ -237,58 +149,6 @@ namespace Proyecto2025.Server.Controllers
                 return NotFound("No members found for this user.");
             }
             return Ok(members);
-        }
-
-        [HttpPost("por-usuario/{UserId}")]
-        public async Task<ActionResult<ChatMember>> CreateChatMemberByUser(ChatMember chatMember)
-        {
-            if (chatMember == null)
-            {
-                return BadRequest("El miembro del chat no puede ser nulo.");
-            }
-            context.ChatMembers.Add(chatMember);
-            await context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetChatMembersByUser), new { userId = chatMember.UserId }, chatMember);
-        }
-
-        [HttpPut("por-usuario/{UserId}")]
-
-        public async Task<IActionResult> UpdateChatMemberByUser(long id, ChatMember chatMember)
-        {
-            if (id != chatMember.Id)
-            {
-                return BadRequest("El ID del miembro del chat no coincide.");
-            }
-            context.Entry(chatMember).State = EntityState.Modified;
-            try
-            {
-                await context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ChatMemberExists(id))
-                {
-                    return NotFound("Miembro de chat no encontrado.");
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return NoContent();
-        }
-
-        [HttpDelete("por-usuario/{UserId}")]
-        public async Task<IActionResult> DeleteChatMemberByUser(long id)
-        {
-            var chatMember = await context.ChatMembers.FindAsync(id);
-            if (chatMember == null)
-            {
-                return NotFound("Miembro de chat no encontrado.");
-            }
-            context.ChatMembers.Remove(chatMember);
-            await context.SaveChangesAsync();
-            return NoContent();
         }
         #endregion
 
@@ -307,58 +167,6 @@ namespace Proyecto2025.Server.Controllers
             return Ok(members);
         }
 
-        [HttpPost("por-chat-usuario/{User}")]
-        public async Task<ActionResult<ChatMember>> CreateChatMemberByChatAndUser(ChatMember chatMember)
-        {
-            if (chatMember == null)
-            {
-                return BadRequest("El miembro del chat no puede ser nulo.");
-            }
-            context.ChatMembers.Add(chatMember);
-            await context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetChatMembersByChatAndUser), new { chatId = chatMember.ChatId, userId = chatMember.UserId }, chatMember);
-        }
-
-        [HttpPut("por-chat-usuario/{User}")]
-
-        public async Task<IActionResult> UpdateChatMemberByChatAndUser(long id, ChatMember chatMember)
-        {
-            if (id != chatMember.Id)
-            {
-                return BadRequest("El ID del miembro del chat no coincide.");
-            }
-            context.Entry(chatMember).State = EntityState.Modified;
-            try
-            {
-                await context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ChatMemberExists(id))
-                {
-                    return NotFound("Miembro de chat no encontrado.");
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return NoContent();
-        }
-
-        [HttpDelete("por-chat-usuario/{User}")]
-        public async Task<IActionResult> DeleteChatMemberByChatAndUser(long id)
-        {
-            var chatMember = await context.ChatMembers.FindAsync(id);
-            if (chatMember == null)
-            {
-                return NotFound("Miembro de chat no encontrado.");
-            }
-            context.ChatMembers.Remove(chatMember);
-            await context.SaveChangesAsync();
-            return NoContent();
-        }
-
         #endregion
 
         #region
@@ -374,57 +182,6 @@ namespace Proyecto2025.Server.Controllers
                 return NotFound("No members found with the specified moderator status.");
             }
             return Ok(members);
-        }
-
-        [HttpPost("por-moderador/{IsModerator}")]
-        public async Task<ActionResult<ChatMember>> CreateChatMemberByModerator(ChatMember chatMember)
-        {
-            if (chatMember == null)
-            {
-                return BadRequest("El miembro del chat no puede ser nulo.");
-            }
-            context.ChatMembers.Add(chatMember);
-            await context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetChatMembersByModerator), new { isModerator = chatMember.IsModerator }, chatMember);
-        }
-
-        [HttpPut("por-moderador/{IsModerator}")]
-        public async Task<IActionResult> UpdateChatMemberByModerator(long id, ChatMember chatMember)
-        {
-            if (id != chatMember.Id)
-            {
-                return BadRequest("El ID del miembro del chat no coincide.");
-            }
-            context.Entry(chatMember).State = EntityState.Modified;
-            try
-            {
-                await context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ChatMemberExists(id))
-                {
-                    return NotFound("Miembro de chat no encontrado.");
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return NoContent();
-        }
-
-        [HttpDelete("por-moderador/{IsModerator}")]
-        public async Task<IActionResult> DeleteChatMemberByModerator(long id)
-        {
-            var chatMember = await context.ChatMembers.FindAsync(id);
-            if (chatMember == null)
-            {
-                return NotFound("Miembro de chat no encontrado.");
-            }
-            context.ChatMembers.Remove(chatMember);
-            await context.SaveChangesAsync();
-            return NoContent();
         }
         #endregion
 
@@ -443,124 +200,7 @@ namespace Proyecto2025.Server.Controllers
             return Ok(members);
         }
 
-        [HttpPost("por-escritura/{CanWrite}")]
-        public async Task<ActionResult<ChatMember>> CreateChatMemberByCanWrite(ChatMember chatMember)
-        {
-            if (chatMember == null)
-            {
-                return BadRequest("El miembro del chat no puede ser nulo.");
-            }
-            context.ChatMembers.Add(chatMember);
-            await context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetChatMembersByCanWrite), new { canWrite = chatMember.CanWrite }, chatMember);
-        }
-
-        [HttpPut("por-escritura/{CanWrite}")]
-        public async Task<IActionResult> UpdateChatMemberByCanWrite(long id, ChatMember chatMember)
-        {
-            if (id != chatMember.Id)
-            {
-                return BadRequest("El ID del miembro del chat no coincide.");
-            }
-            context.Entry(chatMember).State = EntityState.Modified;
-            try
-            {
-                await context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ChatMemberExists(id))
-                {
-                    return NotFound("Miembro de chat no encontrado.");
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return NoContent();
-        }
-
-        [HttpDelete("por-escritura/{CanWrite}")]
-        public async Task<IActionResult> DeleteChatMemberByCanWrite(long id)
-        {
-            var chatMember = await context.ChatMembers.FindAsync(id);
-            if (chatMember == null)
-            {
-                return NotFound("Miembro de chat no encontrado.");
-            }
-            context.ChatMembers.Remove(chatMember);
-            await context.SaveChangesAsync();
-            return NoContent();
-        }
         #endregion
-
-        #region
-        [HttpGet("por-cuandoseunio/{JoinedAt}")]
-        public async Task<ActionResult<IEnumerable<ChatMember>>> GetChatMembersByJoinedAt(DateTime joinedAt)
-        {
-            var members = await context.ChatMembers
-                .Include(cm => cm.User) // Incluye la entidad User relacionada
-                .Where(cm => cm.JoinedAt.Date == joinedAt.Date)
-                .ToListAsync();
-            if (members == null || members.Count == 0)
-            {
-                return NotFound("No members found with the specified join date.");
-            }
-            return Ok(members);
-        }
-
-        [HttpPost("por-cuandoseunio/{JoinedAt}")]
-        public async Task<ActionResult<ChatMember>> CreateChatMemberByJoinedAt(ChatMember chatMember)
-        {
-            if (chatMember == null)
-            {
-                return BadRequest("El miembro del chat no puede ser nulo.");
-            }
-            context.ChatMembers.Add(chatMember);
-            await context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetChatMembersByJoinedAt), new { joinedAt = chatMember.JoinedAt }, chatMember);
-        }
-
-        [HttpPut("por-cuandoseunio/{JoinedAt}")]
-        public async Task<IActionResult> UpdateChatMemberByJoinedAt(long id, ChatMember chatMember)
-        {
-            if (id != chatMember.Id)
-            {
-                return BadRequest("El ID del miembro del chat no coincide.");
-            }
-            context.Entry(chatMember).State = EntityState.Modified;
-            try
-            {
-                await context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ChatMemberExists(id))
-                {
-                    return NotFound("Miembro de chat no encontrado.");
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return NoContent();
-        }
-
-        [HttpDelete("por-cuandoseunio/{JoinedAt}")]
-        public async Task<IActionResult> DeleteChatMemberByJoinedAt(long id)
-        {
-            var chatMember = await context.ChatMembers.FindAsync(id);
-            if (chatMember == null)
-            {
-                return NotFound("Miembro de chat no encontrado.");
-            }
-            context.ChatMembers.Remove(chatMember);
-            await context.SaveChangesAsync();
-            return NoContent();
-        }
     }
-        #endregion
 
 }
