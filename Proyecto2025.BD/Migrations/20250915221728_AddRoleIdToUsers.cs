d13da7d4ad10c872c62c13c752715eda1cd1097d
frannpineroo <frannpinero@hotmail.com>
2025-09-15 21:37:16 -0300

    creando la page para ver la lista de usuarios, todavia no funciona

---
+++ Proyecto2025.BD/Migrations/20250915221728_AddRoleIdToUsers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto2025.BD.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoleIdToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
